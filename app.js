dac01f3230e88b839779f7cbb9dde0224bda327a
Vivek Patel <40218578+vvek87@users.noreply.github.com>
2018-09-11 12:46:23 -0500

    add code for date menu and priority menu

--- app.js
+++ app.js
@@ -6,17 +6,23 @@ $(document).ready(function() {
     // store values
     let inputKey = $(".user-input-title").val();
     let inputValue = $(".user-input-body").val();
+    let inputDate = $(".user-due-date").val();   //new
+    let inputDropDown = $(".user-drop-down-menu").val();         // new
 
     // clear values
     $(".user-input-title").val("");
     $(".user-input-body").val("");
+    $(".user-due-date").val("");         //new
+    $(".user-drop-down-menu").prop('selectedIndex',0);  //new
 
     console.log(inputKey, inputValue);
 
     localStorage.setItem(inputKey, inputValue);
+    localStorage.setItem("date", inputDate);
+    localStorage.setItem("priority", inputDropDown);
     // data-
-    let itemHtml = '<div class="display-item" data-storage-key="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + '</div>';
-    $(".display").html(itemHtml);
+    let itemHtml = '<div class="display-item" data-storage="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + ' ' + localStorage.getItem("date") + ' ' + localStorage.getItem("priority") + ' </div>';
+    $(".display").append(itemHtml);
     //console.log(localStorage);
     // how can we delegate this event to the outer html node?
     // https://learn.jquery.com/events/event-delegation/
@@ -25,12 +31,12 @@ $(document).ready(function() {
       // plop the key:value back into the input boxes
 
       // get the values from the the divs?
-      console.log("key=> ", e.target.dataset.storageKey); // user-input-title
-      localStorage.getItem(e.target.dataset.storageKey); // user-input-body
-
+      console.log("key=> ", e.target.dataset.storage); // user-input-title
+      localStorage.getItem(e.target.dataset.storage); // user-input-body
+      
       // set those values in the form fields
-      $(".user-input-title").val(e.target.dataset.storageKey);
-      $(".user-input-body").val(localStorage.getItem(e.target.dataset.storageKey));
+      $(".user-input-title").val(e.target.dataset.storage);
+      $(".user-input-body").val(localStorage.getItem(e.target.dataset.storage));
     });
 
   });
@@ -47,6 +53,9 @@ $(document).ready(function() {
    $(".del-text-btn").on("click", function() {
      alert('item deleted? check the console'); // maybe change to a window.confirm
      localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
+     var selectedItem =  $(".display-item[data-storage = '"+$(".user-input-title").val()+"']")
+     console.log(selectedItem[0].dataset.storage);+
+     selectedItem[0].remove();
      $(".user-input-title").val("");
      $(".user-input-body").val("");
      // clearing display? what if I have multiple items?
