25045286bc2a1901415d00345168e5d71be65984
Vivek Patel <40218578+vvek87@users.noreply.github.com>
2018-09-13 12:52:17 -0500

    mvp features implemented for crud

--- app.js
+++ app.js
@@ -1,74 +1,96 @@
 $(document).ready(function() {
+  var editActive = false;
+  var tempTitle = '';
 
-
-  $(".add-text-btn").on("click", function(){
-
-    // store values
-    let inputKey = $(".user-input-title").val();
-    let inputValue = $(".user-input-body").val();
-    let inputDate = $(".user-due-date").val();   //new
-    let inputDropDown = $(".user-drop-down-menu").val();         // new
-
-    // clear values
-    $(".user-input-title").val("");
-    $(".user-input-body").val("");
-    $(".user-due-date").val("");         //new
-    $(".user-drop-down-menu").prop('selectedIndex',0);  //new
-
-    console.log(inputKey, inputValue);
-
-    localStorage.setItem(inputKey, inputValue);
-    localStorage.setItem("date", inputDate);
-    localStorage.setItem("priority", inputDropDown);
-    // data-
-    let itemHtml = '<div class="display-item" data-storage="'+inputKey+'"> ' + inputKey + ' ' +  localStorage.getItem(inputKey) + ' ' + localStorage.getItem("date") + ' ' + localStorage.getItem("priority") + ' </div>';
-    $(".display").append(itemHtml);
-    //console.log(localStorage);
-    // how can we delegate this event to the outer html node?
-    // https://learn.jquery.com/events/event-delegation/
-
-    $(".display-item").on("click", function(e){
-      // plop the key:value back into the input boxes
-
-      // get the values from the the divs?
-      console.log("key=> ", e.target.dataset.storage); // user-input-title
-      localStorage.getItem(e.target.dataset.storage); // user-input-body
-      
-      // set those values in the form fields
-      $(".user-input-title").val(e.target.dataset.storage);
-      $(".user-input-body").val(localStorage.getItem(e.target.dataset.storage));
-    });
-
+  $('.createTodo').click(function() {
+      $('.createList').toggle(125);
+    
   });
 
+  $('.submitNewTodo').click(function() {
+        if (editActive) {
+
+      $(`tr[data-storage="${tempTitle}"]`).remove();
+      }
+
+      var inputTitle = $(".inputTitle").val();
+      var inputDescription = $(".inputDescription").val();
+      var inputDate = $(".inputDate").val();
+      var inputPriority = $(".inputPriority").val();
+      
+      $(".inputTitle").val("");
+      $(".inputDescription").val("");
+      $(".inputDate").val("");
+      $(".inputPriority").prop("selectedIndex", 0);
+      
+      var deleteTodo = `<button type="button" class="deleteButton">Delete</button>`;
+      var editTodo = `<button type="button" class="editButton">Edit</button>`;
+           // use ` (backtick) before and after statement to replace quotes and use ${variable} for variables
+      if (inputPriority === "Low") {
+        var singleTodoList = `<tr class="newSingleTodo Low" data-storage="${inputTitle}"><td>${inputTitle}</td><td>${inputDescription}</td><td class="countDown" title="countdown timer">${inputDate}</td><td>${inputPriority}</td><td>${editTodo}${deleteTodo}</td></tr>`;
+
+      };
+
+       if (inputPriority === "Medium") {
+        var singleTodoList = `<tr class="newSingleTodo Medium" data-storage="${inputTitle}"><td>${inputTitle}</td><td>${inputDescription}</td><td class="countDown" title="countdown timer">${inputDate}</td><td>${inputPriority}</td><td>${editTodo}${deleteTodo}</td></tr>`;
+
+      };
+
+       if (inputPriority === "High") {
+        var singleTodoList = `<tr class="newSingleTodo High" data-storage="${inputTitle}"><td>${inputTitle}</td><td>${inputDescription}</td><td class="countDown" title="countdown timer">${inputDate}</td><td>${inputPriority}</td><td>${editTodo}${deleteTodo}</td></tr>`;
+
+      };
+
+      // var singleTodoList = `<tr class="newSingleTodo" data-storage="${inputTitle}"><td>${inputTitle}</td><td>${inputDescription}</td><td>${inputDate}</td><td>${inputPriority}</td><td>${editTodo}${deleteTodo}</td></tr>`;
+      $(".todoList").append(singleTodoList);
+
+      $('.createList').toggle(125);
+    
+  });
+
+  $(".todoList").on("click", ".deleteButton", function() {
+    var deletion = false;
+    if (confirm('Confirm delete')) {
+      deletion = true;
+    }  
+    else { 
+      deletion = false;
+    }
+    if (deletion === true) {
+      $(this).closest("tr").remove();
+    }
+  });
+
+  $(".inputDate").attr('title', 'this should replace original');
+
+  // $( ".todoList" ).on("mouseover", ".countDown", function(event) {
+  // // alert("hello"); 
+  // });
+
+  //   $( ".todoList" ).on("mouseleave", ".countDown", function(event) {
+  // // alert("hello");
+  // alert("leave");
+  // });
 
 
-   // TODO add back in later
-   // $(".user-input").on("keyup", function(){
-   //   let inputValue = $(".user-input").val();
-   //   localStorage.setItem("testStorage", inputValue);
-   //   $(".display").text(localStorage.getItem("testStorage"));
-   // });
+  $(".todoList").on("click", ".editButton", function(e) {
 
-   $(".del-text-btn").on("click", function() {
-     alert('item deleted? check the console'); // maybe change to a window.confirm
-     localStorage.removeItem( $('.user-input-title').val() ); // grab the title and plop here
-     var selectedItem =  $(".display-item[data-storage = '"+$(".user-input-title").val()+"']")
-     console.log(selectedItem[0].dataset.storage);+
-     selectedItem[0].remove();
-     $(".user-input-title").val("");
-     $(".user-input-body").val("");
-     // clearing display? what if I have multiple items?
-     // after item is removed from local storage, redisplay items from local storage
-     // refresh from storage?
-   });
+      $('.createList').toggle(125);
 
+    var inputTitle = this.parentElement.parentElement.children[0].innerText;
+    var inputDescription = this.parentElement.parentElement.children[1].innerText;
+    var inputDate = this.parentElement.parentElement.children[2].innerText;
+    var inputPriority = this.parentElement.parentElement.children[3].innerText;
+    
+    $(".inputTitle").val(inputTitle);
+    $(".inputDescription").val(inputDescription);
+    $(".inputDate").val(inputDate);
+    $(".inputPriority").val(inputPriority);
 
-   // iterative approach to adding items
-   // store data as stringified array of objects
-   // store data with individual keys
-  // how do we get keys? research Object.keys
-
+    // $(`tr[data-storage="${inputTitle}"]`).remove();
+    editActive = true;
+    tempTitle = inputTitle;
+  });
 
 
 });
